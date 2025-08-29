92d538a77bbff4b2c7c801829a001a66efa8817d
lavudyaraja <lavudyaraja12345@gmail.com>
2025-08-29 20:11:20 +0530

    Fix resizable panels ref issue and update sidebar

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,5 +1,6 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider , ThemeProviderProps} from "next-themes"
+// import { ThemeProvider, type ThemeProviderProps } from "next-themes"
+
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
